f64228ddec7e927cfcf4a245c8f8e426a097495e
tanvigirase <tanvigirase93@gmail.com>
2025-11-06 22:08:56 +0530

    update

--- app/main.py
+++ app/main.py
@@ -1,10 +1,25 @@
 from flask import Flask
 
-app = Flask(__name__)
+
+
+
+
+app = Flask(_name_)
+
+
+
+
 
 @app.route('/')
+
 def hello():
+
     return "Hello from OpenShift CI/CD Pipeline!"
 
-if __name__ == "__main__":
+
+
+
+
+if _name_ == "_main_":
+
     app.run(host="0.0.0.0", port=8080)
